0d641758cfba540e3933dc46f124778c358f9b4a
parmezanik <tomas.lajda0@gmail.com>
2021-07-09 13:39:21 +0200

    Logical argument about Socrates

---
+++ if/socrates.js
@@ -0,0 +1,12 @@
+"use strict";
+
+const areMenMortal = true;
+const isSocratesMan = true;
+
+if (areMenMortal && isSocratesMan === true) {
+  console.log("Socrates is mortal.");
+} else if (areMenMortal === false) {
+  console.log("No man can die!!!");
+} else {
+  console.log("Socrates can't die!!!");
+}
