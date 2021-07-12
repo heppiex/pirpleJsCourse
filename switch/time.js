63a2a0fa674e201da670a090579ef905c371fb79
parmezanik <tomas.lajda0@gmail.com>
2021-07-12 21:59:48 +0200

    Invalid inputs handling in timeAdder

--- switch/time.js
+++ switch/time.js
@@ -21,14 +21,52 @@ const typeCheck = function (checker) {
   }
 };
 
+const numberCheck = (number) => {
+  if (typeof number === "number") {
+    return true;
+  } else return false;
+};
+
 const timeAdder = function (timeOne, typeOne, timeTwo, typeTwo) {
-  typeCheck(typeOne);
-  typeCheck(typeTwo);
-  if (typeCheck(typeOne) === false) {
-    console.log(`This is invalid because the first argument is the wrong type`);
+  if (arguments.length < 4) {
+    return console.log(
+      `This is invalid because you need to pass 4 arguments not ${arguments.length}`
+    );
+  } else if (typeCheck(typeOne) === false) {
+    return console.log(
+      `This is invalid because the first argument is the wrong type`
+    );
   } else if (typeCheck(typeTwo) === false) {
-    console.log(
+    return console.log(
       `This is invalid because the second argument is the wrong type`
+    );
+  } else if (numberCheck(timeOne) === false) {
+    returnconsole.log(
+      ` This is invalid because the first argument is the wrong type`
+    );
+  } else if (numberCheck(timeTwo) === false) {
+    return console.log(
+      ` This is invalid because the trird argument is the wrong type`
+    );
+  } else if (
+    timeOne > 1 &&
+    (typeOne === "second" ||
+      typeOne === "minute" ||
+      typeOne === "hour" ||
+      typeOne === "day")
+  ) {
+    return console.log(
+      `This is impossible because "${typeOne}" is singular and ${timeOne} is plural`
+    );
+  } else if (
+    timeTwo > 1 &&
+    (typeTwo === "second" ||
+      typeTwo === "minute" ||
+      typeTwo === "hour" ||
+      typeTwo === "day")
+  ) {
+    return console.log(
+      `This is impossible because "${typeTwo}" is singular and ${timeTwo} is plural`
     );
   }
 };
