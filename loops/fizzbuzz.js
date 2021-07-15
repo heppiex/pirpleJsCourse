10aa008db4afb2109d7b246abfe35caf3a856f9c
parmezanik <tomas.lajda0@gmail.com>
2021-07-15 21:08:48 +0200

    Full version fizzbuzz program

--- loops/fizzbuzz.js
+++ loops/fizzbuzz.js
@@ -1,1 +1,24 @@
 "use strict";
+
+const primeCheck = function (number) {
+  for (let j = 2; j <= number; j++) {
+    if (number % j === 0 && number != j) {
+      return false;
+    }
+  }
+  return true;
+};
+
+for (let i = 1; i <= 100; i++) {
+  if (i % 3 === 0 && i % 5 === 0) {
+    console.log("FizzBuzz");
+  } else if (i % 3 === 0) {
+    console.log("Fizz");
+  } else if (i % 5 === 0) {
+    console.log("Buzz");
+  } else if (primeCheck(i) && i != 1) {
+    console.log("Prime");
+  } else {
+    console.log(i);
+  }
+}
