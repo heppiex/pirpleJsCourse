ad91e2b677b1012ea9bc5ec7da1d5b70cdd809ea
parmezanik <tomas.lajda0@gmail.com>
2021-07-19 17:32:04 +0200

    colors for triangels

--- DOM/script.js
+++ DOM/script.js
@@ -1,1 +1,14 @@
 "use strict";
+
+const colors = {
+  yellow: "hsl(63, 100%, 50%)",
+  blue: "hsl(223, 100%, 50%)",
+  pink: "hsl(311, 100%, 50%)",
+  red: "hsl(0, 100%, 50%)",
+  purple: "hsl(279, 100%, 50%)",
+  cyan: "hsl(185, 100%, 50%)",
+  lime: "hsl(128, 100%, 50%)",
+  green: "hsl(117, 100%, 35%)",
+  orange: "hsl(43, 100%, 50%)",
+  brown: "hsl(26, 53%, 49%)",
+};
