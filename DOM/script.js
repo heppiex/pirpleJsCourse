d479d80e96878052b669e9c0f6223f2949b6b203
parmezanik <tomas.lajda0@gmail.com>
2021-07-19 18:11:36 +0200

    Created element constructor

--- DOM/script.js
+++ DOM/script.js
@@ -1,5 +1,6 @@
 "use strict";
 
+// Colors for rectangles
 const colors = {
   yellow: "hsl(63, 100%, 50%)",
   blue: "hsl(223, 100%, 50%)",
@@ -12,3 +13,44 @@ const colors = {
   orange: "hsl(43, 100%, 50%)",
   brown: "hsl(26, 53%, 49%)",
 };
+
+const elementConstructor = function (
+  what = "p",
+  text = null,
+  where = "body",
+  elementClass = null,
+  elementId = null
+) {
+  if (typeof what != "string" || typeof where != "string") {
+    console.log("You pass in elementConstructor bad type!");
+    return 0;
+  }
+
+  const element = document.createElement(what);
+
+  //checking for value
+  if (typeof text === "string") {
+    element.innerHTML = text;
+  }
+
+  const lisener = document.querySelector(where);
+  lisener.appendChild(element);
+
+  //checking for value
+  if (typeof elementClass === "string") {
+    element.setAttribute("class", elementClass);
+  }
+
+  //checking for value
+  if (typeof elementId === "string") {
+    element.setAttribute("id", elementId);
+  }
+};
+
+elementConstructor("h1", "Rectangles, just rectangles", "body");
+elementConstructor("title", "Rectangles, just rectangles", "head");
+elementConstructor(
+  "h2",
+  "Rectangles lot of them, I don't know why are you still here.",
+  "body"
+);
